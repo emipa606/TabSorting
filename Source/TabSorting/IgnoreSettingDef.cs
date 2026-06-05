194f81aea801227c59c8713885cbe46327243689
keeptpa <keeptpa@qq.com>
2026-06-05 16:22:50 +0800

    feat: add a new setting could let other mod author add ignore item on their side, not our side.

---
+++ Source/TabSorting/IgnoreSettingDef.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace TabSorting;
+
+public class IgnoreSettingDef : Def
+{
+    public List<string> modIDsToIgnore;
+    public List<string> defsToIgnore;
+
+    public override void ResolveReferences()
+    {
+        base.ResolveReferences();
+        if (modIDsToIgnore?.Any() == true)
+            modIDsToIgnore = modIDsToIgnore.Select(id => id.ToLower()).ToList();
+    }
+}
